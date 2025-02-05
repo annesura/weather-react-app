ef4b9f6099b33f87750fd70d967087b5dc6951c8
annesura <annesura00@gmail.com>
2025-02-05 14:42:28 -0500

    Added Weather component

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    `The weather in ${response.data.city} is ${response.data.temperature.current} degrees`;
+  }
+  let apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+  return;
+}
