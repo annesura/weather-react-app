286c0c292e89c20a5d17ff1f06a0b7dec6791d35
annesura <annesura00@gmail.com>
2025-02-05 18:11:50 -0500

    Added footer
    Links to GitHub repository and Netlify link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
       <header className="App-header">
         <Weather />
       </header>
+      <footer>
+        This project is{" "}
+        <a href="https://github.com/annesura/weather-react-app">
+          open-sourced on Git-Hub
+        </a>
+        , and <a href="funny-llama-3e0b51.netlify.app">hosted on Netlify</a>.
+      </footer>
     </div>
   );
 }
